b18767171efe743cb26ea0eba4834af47970cf60
Patternscompany <pandureddypatterns@gmail.com>
2025-10-22 11:20:27 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,185 @@
+(function ($) {
+  "use strict";
+
+  // Dropdown on mouse hover
+  $(document).ready(function () {
+    function toggleNavbarMethod() {
+      if ($(window).width() > 992) {
+        $(".navbar .dropdown")
+          .on("mouseover", function () {
+            $(".dropdown-toggle", this).trigger("click");
+          })
+          .on("mouseout", function () {
+            $(".dropdown-toggle", this).trigger("click").blur();
+          });
+      } else {
+        $(".navbar .dropdown").off("mouseover").off("mouseout");
+      }
+    }
+    toggleNavbarMethod();
+    $(window).resize(toggleNavbarMethod);
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
+
+  // Date and time picker
+  $("#date").datetimepicker({
+    format: "L",
+  });
+  $("#time").datetimepicker({
+    format: "LT",
+  });
+
+  // Service carousel
+  $(".service-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    loop: true,
+    dots: false,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      },
+      1200: {
+        items: 5,
+      },
+    },
+  });
+
+  // Pricing carousel
+  $(".pricing-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    loop: true,
+    dots: false,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+    },
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    dots: true,
+    loop: true,
+    items: 1,
+  });
+})(jQuery);
+
+//when scroll>100 then navabar fixed to top
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll > 100) {
+    $(".navbar").addClass("active");
+  } else {
+    $(".navbar").removeClass("active");
+  }
+});
+
+/**
+ * add event on element
+ */
+
+// const addEventOnElem = function (elem, type, callback) {
+//   if (elem.length > 1) {
+//     for (let i = 0; i < elem.length; i++) {
+//       elem[i].addEventListener(type, callback);
+//     }
+//   } else {
+//     elem.addEventListener(type, callback);
+//   }
+// };
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem instanceof NodeList || elem instanceof HTMLCollection) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else if (elem) {
+    elem.addEventListener(type, callback);
+  }
+};
+
+
+const filterBtns = document.querySelectorAll("[data-filter-btn]");
+const filterItems = document.querySelectorAll("[data-filter]");
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+  lastClickedFilterBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedFilterBtn = this;
+
+  for (let i = 0; i < filterItems.length; i++) {
+    if (
+      this.dataset.filterBtn === filterItems[i].dataset.filter ||
+      this.dataset.filterBtn === "all"
+    ) {
+      filterItems[i].style.display = "block";
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].style.display = "none";
+      filterItems[i].classList.remove("active");
+    }
+  }
+};
+
+addEventOnElem(filterBtns, "click", filter);
+//remove .html extension when hover
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".navbar-nav a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("mouseover", function () {
+      const originalHref = link.getAttribute("href");
+      const cleanHref = originalHref.replace(".html", "");
+      link.setAttribute("href", cleanHref);
+    });
+
+    link.addEventListener("mouseout", function () {
+      const originalHref = link.getAttribute("href");
+      const restoreHref = originalHref + ".html";
+      link.setAttribute("href", restoreHref);
+    });
+  });
+});
